e8b58613acf139c486a105737b16205008005dd6
Abhinav Saini <abhinavsaini124@gmail.com>
2022-10-14 01:01:56 +0530

    Revert "Updated BurnBook.py"

--- BurnBook.py
+++ BurnBook.py
@@ -4,7 +4,7 @@ from tkinter import messagebox
 import pymysql
 
 # Add your own database name and password here to reflect in the code
-mypass = "abhii"
+mypass = "abhi"
 mydatabase="db"
 
 con = pymysql.connect(host="localhost",user="root",password=mypass,database=mydatabase)
